22a451f53e43c93284be3843d29d68e1e2f17c16
nowido <nowido@mail.ru>
2016-11-26 14:44:59 +0300

    Update hs.js

--- hs.js
+++ hs.js
@@ -28,57 +28,32 @@ var yad = require('./modules/yad.js').init(yadToken);
 //------------------------------------------------------------------------------
 
 var cachedFiles = 
-[
-    {mode: 'bin', path: './jquery/jquery.js.gz'},
-    {mode: 'text', path: './jquery/jquery.js'},
-    {mode: 'text', path: './scripts/master-injection.js'},
-    {mode: 'text', path: './scripts/client-api.js'},
-    {mode: 'text', path : './scripts/station.js'},
-    {mode: 'text', path : './scripts/task.js'},
-    {mode: 'text', path : './pages-elements/pagestart'},
-    {mode: 'text', path : './pages-elements/page-station.html'},
-    {mode: 'text', path : './pages-elements/page-mi.html'},
-    {mode: 'text', path : './pages-elements/page-task.html'},
-    {mode: 'text', path : './pages-elements/pagend-1'},
-    {mode: 'text', path : './pages-elements/pagend-2'},
-    {mode: 'text', path : './pages-elements/pagend-nosio'},
-    {mode: 'bin', path : './bootstrap/css/bootstrap.min.css.gz'},
-    {mode: 'text', path : './bootstrap/css/bootstrap.min.css'},
-    {mode: 'bin', path : './bootstrap/js/bootstrap.min.js.gz'},
-    {mode: 'text', path : './bootstrap/js/bootstrap.min.js'},
-    {mode: 'bin', path : './bootstrap/fonts/glyphicons-halflings-regular.woff2'},
-    {mode: 'bin', path : './bootstrap/fonts/glyphicons-halflings-regular.woff'},
-    {mode: 'bin', path : './bootstrap/fonts/glyphicons-halflings-regular.ttf'},
-    {mode: 'bin', path : './bootstrap/fonts/glyphicons-halflings-regular.svg.gz'},
-    {mode: 'text', path : './bootstrap/fonts/glyphicons-halflings-regular.svg'},
-    {mode: 'bin', path : './bootstrap/fonts/glyphicons-halflings-regular.eot'},
-    {mode: 'text', path : './scripts/vmbuilder-bs.js'}
-];
-
-const jqgzip        = 0;
-const jqnogzip      = 1;
-const miscript      = 2;
-const cliapis       = 3;
-const station       = 4;
-const task          = 5;
-const pagestart     = 6;
-const pagestation   = 7;
-const pagemi        = 8;
-const pagetask      = 9;
-const pagend1       = 10;
-const pagend2       = 11;
-const pagendnosio   = 12;
-const bscssgzip     = 13;
-const bscssnogzip   = 14;
-const bsjsgzip      = 15;
-const bsjsnogzip    = 16;
-const bsfontwoff2   = 17;
-const bsfontwoff    = 18;
-const bsfonttf      = 19;
-const bsfontsvgzip  = 20;
-const bsfontsvg     = 21;
-const bsfonteot     = 22;
-const vmbuilderbs   = 23;
+{
+    'jqgzip':       {mode: 'bin',   path: './jquery/jquery.js.gz'},
+    'jqnogzip':     {mode: 'text',  path: './jquery/jquery.js'},
+    'miscript':     {mode: 'text',  path: './scripts/master-injection.js'},
+    'cliapis':      {mode: 'text',  path: './scripts/client-api.js'},
+    'station':      {mode: 'text',  path : './scripts/station.js'},
+    'task':         {mode: 'text',  path : './scripts/task.js'},
+    'pagestart':    {mode: 'text',  path : './pages-elements/pagestart'},
+    'pagestation':  {mode: 'text',  path : './pages-elements/page-station.html'},
+    'pagemi':       {mode: 'text',  path : './pages-elements/page-mi.html'},
+    'pagetask':     {mode: 'text',  path : './pages-elements/page-task.html'},
+    'pagend1':      {mode: 'text',  path : './pages-elements/pagend-1'},
+    'pagend2':      {mode: 'text',  path : './pages-elements/pagend-2'},
+    'pagendnosio':  {mode: 'text',  path : './pages-elements/pagend-nosio'},
+    'bscssgzip':    {mode: 'bin',   path : './bootstrap/css/bootstrap.min.css.gz'},
+    'bscssnogzip':  {mode: 'text',  path : './bootstrap/css/bootstrap.min.css'},
+    'bsjsgzip':     {mode: 'bin',   path : './bootstrap/js/bootstrap.min.js.gz'},
+    'bsjsnogzip':   {mode: 'text',  path : './bootstrap/js/bootstrap.min.js'},
+    'bsfontwoff2':  {mode: 'bin',   path : './bootstrap/fonts/glyphicons-halflings-regular.woff2'},
+    'bsfontwoff':   {mode: 'bin',   path : './bootstrap/fonts/glyphicons-halflings-regular.woff'},
+    'bsfonttf':     {mode: 'bin',   path : './bootstrap/fonts/glyphicons-halflings-regular.ttf'},
+    'bsfontsvgzip': {mode: 'bin',   path : './bootstrap/fonts/glyphicons-halflings-regular.svg.gz'},
+    'bsfontsvg':    {mode: 'text',  path : './bootstrap/fonts/glyphicons-halflings-regular.svg'},
+    'bsfonteot':    {mode: 'bin',   path : './bootstrap/fonts/glyphicons-halflings-regular.eot'},
+    'vmbuilderbs':  {mode: 'text',  path : './scripts/vmbuilder-bs.js'}
+};
 
 //------------------------------------------------------------------------------
 
@@ -108,50 +83,40 @@ const ctSvgGz   = {'Content-Type': svgType, 'Content-Encoding': 'gzip'};
 //------------------------------------------------------------------------------
 
 var staticMap = 
-[
+{
+    'jqmap':
     {
-        'gz': {contentType: ctTextGz, contentId: jqgzip}, 
-        'nogzip': {contentType: ctText, contentId: jqnogzip}
+        'gz': {contentType: ctTextGz, contentId: 'jqgzip'}, 
+        'nogzip': {contentType: ctText, contentId: 'jqnogzip'}
     },
+    'bscssmap':
     {
-        'gz': {contentType: ctCssGz, contentId: bscssgzip}, 
-        'nogzip': {contentType: ctCss, contentId: bscssnogzip}
+        'gz': {contentType: ctCssGz, contentId: 'bscssgzip'}, 
+        'nogzip': {contentType: ctCss, contentId: 'bscssnogzip'}
     },
+    'bsjsmap':
     {
-        'gz': {contentType: ctTextGz, contentId: bsjsgzip}, 
-        'nogzip': {contentType: ctText, contentId: bsjsnogzip}
+        'gz': {contentType: ctTextGz, contentId: 'bsjsgzip'}, 
+        'nogzip': {contentType: ctText, contentId: 'bsjsnogzip'}
     },
+    'bsfontsvgmap':
     {
-        'gz': {contentType: ctSvgGz, contentId: bsfontsvgzip}, 
-        'nogzip': {contentType: ctSvg, contentId: bsfontsvg}
+        'gz': {contentType: ctSvgGz, contentId: 'bsfontsvgzip'}, 
+        'nogzip': {contentType: ctSvg, contentId: 'bsfontsvg'}
     }
-];
-
-const jqmap         = 0;
-const bscssmap      = 1;
-const bsjsmap       = 2;
-const bsfontsvgmap  = 3;
+};
 
 var staticMap2 = 
-[
-    {contentType: ctWoff, contentId: bsfontwoff}, 
-    {contentType: ctWoff2, contentId: bsfontwoff2},
-    {contentType: ctTtf, contentId: bsfonttf},
-    {contentType: ctEot, contentId: bsfonteot}
-];
-
-const bswoffmap     = 0;
-const bswoff2map    = 1;
-const bsttfmap      = 2;
-const bseotmap      = 3;
+{
+    'bswoffmap': {contentType: ctWoff, contentId: 'bsfontwoff'}, 
+    'bswoff2map': {contentType: ctWoff2, contentId: 'bsfontwoff2'},
+    'bsttfmap': {contentType: ctTtf, contentId: 'bsfonttf'},
+    'bseotmap': {contentType: ctEot, contentId: 'bsfonteot'}
+};
 
 //------------------------------------------------------------------------------
 
-var composedPages = [];
-
-const mipagecomp        = 0;
-const stationpagecomp   = 1;
-const taskpagecomp      = 2;
+var composedPages = {};
 
 //------------------------------------------------------------------------------
 
@@ -179,21 +144,21 @@ function serveComposedPages(req, res)
 
 //------------------------------------------------------------------------------
 
-exapp.get('/jquery.js', serveStatic.bind(staticMap[jqmap]));
-exapp.get('/css/bootstrap.min.css', serveStatic.bind(staticMap[bscssmap]));
-exapp.get('/js/bootstrap.min.js', serveStatic.bind(staticMap[bsjsmap]));
-exapp.get('/fonts/glyphicons-halflings-regular.svg', serveStatic.bind(staticMap[bsfontsvgmap]));
+exapp.get('/jquery.js', serveStatic.bind(staticMap['jqmap']));
+exapp.get('/css/bootstrap.min.css', serveStatic.bind(staticMap['bscssmap']));
+exapp.get('/js/bootstrap.min.js', serveStatic.bind(staticMap['bsjsmap']));
+exapp.get('/fonts/glyphicons-halflings-regular.svg', serveStatic.bind(staticMap['bsfontsvgmap']));
 
-exapp.get('/fonts/glyphicons-halflings-regular.woff2', serveStatic2.bind(staticMap2[bswoff2map]));
-exapp.get('/fonts/glyphicons-halflings-regular.woff', serveStatic2.bind(staticMap2[bswoffmap]));
-exapp.get('/fonts/glyphicons-halflings-regular.ttf', serveStatic2.bind(staticMap2[bsttfmap]));
-exapp.get('/fonts/glyphicons-halflings-regular.eot', serveStatic2.bind(staticMap2[bseotmap]));
+exapp.get('/fonts/glyphicons-halflings-regular.woff2', serveStatic2.bind(staticMap2['bswoff2map']));
+exapp.get('/fonts/glyphicons-halflings-regular.woff', serveStatic2.bind(staticMap2['bswoffmap']));
+exapp.get('/fonts/glyphicons-halflings-regular.ttf', serveStatic2.bind(staticMap2['bsttfmap']));
+exapp.get('/fonts/glyphicons-halflings-regular.eot', serveStatic2.bind(staticMap2['bseotmap']));
 
 //------------------------------------------------------------------------------
 
-exapp.get('/', serveComposedPages.bind({index: stationpagecomp}));
-exapp.get('/mi', serveComposedPages.bind({index: mipagecomp}));
-exapp.get('/task', serveComposedPages.bind({index: taskpagecomp}));
+exapp.get('/', serveComposedPages.bind({index: 'stationpagecomp'}));
+exapp.get('/mi', serveComposedPages.bind({index: 'mipagecomp'}));
+exapp.get('/task', serveComposedPages.bind({index: 'taskpagecomp'}));
 
 //------------------------------------------------------------------------------
 
@@ -205,11 +170,11 @@ exapp.post('/', (req, res) =>
             .then(postedContent => 
             {
                 var pageContent = 
-                    cachedFiles[pagestart].content +
-                    cachedFiles[pagend1].content + 
-                    cachedFiles[cliapis].content + '\n' +
+                    cachedFiles['pagestart'].content +
+                    cachedFiles['pagend1'].content + 
+                    cachedFiles['cliapis'].content + '\n' +
                     postedContent + '\n' +
-                    cachedFiles[pagend2].content;
+                    cachedFiles['pagend2'].content;
                 
                 res.set(ctHtml);
                 res.send(pageContent);    
@@ -240,9 +205,11 @@ var asyncInitializationPhases =
     asyncrw.cached(cachedFiles)
         .then(files => 
         {
+            var keys = Object.keys(cachedFiles);
+            
             files.forEach((content, index) => 
             {
-                var entry = cachedFiles[index];
+                var entry = cachedFiles[keys[index]];
                 entry.content = content;
                 
                 console.log('cached ' + entry.path + ' (' + content.length + ' bytes)');
@@ -266,29 +233,29 @@ Promise.all(asyncInitializationPhases).then(() =>
     
     siocomm.init(server, channel, commandsRegistry);
     
-    composedPages[mipagecomp] = 
-        cachedFiles[pagestart].content +
-        cachedFiles[pagemi].content +
-        cachedFiles[pagendnosio].content + 
-        cachedFiles[miscript].content + '\n' +
-        cachedFiles[pagend2].content;
+    composedPages['mipagecomp'] = 
+        cachedFiles['pagestart'].content +
+        cachedFiles['pagemi'].content +
+        cachedFiles['pagendnosio'].content + 
+        cachedFiles['miscript'].content + '\n' +
+        cachedFiles['pagend2'].content;
     
-    composedPages[stationpagecomp] = 
-        cachedFiles[pagestart].content +
-        cachedFiles[pagestation].content +
-        cachedFiles[pagend1].content + 
-        cachedFiles[cliapis].content + '\n' +
-        cachedFiles[station].content + '\n' +
-        cachedFiles[pagend2].content;
+    composedPages['stationpagecomp'] = 
+        cachedFiles['pagestart'].content +
+        cachedFiles['pagestation'].content +
+        cachedFiles['pagend1'].content + 
+        cachedFiles['cliapis'].content + '\n' +
+        cachedFiles['station'].content + '\n' +
+        cachedFiles['pagend2'].content;
 
-    composedPages[taskpagecomp] = 
-        cachedFiles[pagestart].content +
-        cachedFiles[pagetask].content +
-        cachedFiles[pagend1].content + 
-        cachedFiles[cliapis].content + '\n' +
-        cachedFiles[vmbuilderbs].content + '\n' +
-        cachedFiles[task].content + '\n' +
-        cachedFiles[pagend2].content;
+    composedPages['taskpagecomp'] = 
+        cachedFiles['pagestart'].content +
+        cachedFiles['pagetask'].content +
+        cachedFiles['pagend1'].content + 
+        cachedFiles['cliapis'].content + '\n' +
+        cachedFiles['vmbuilderbs'].content + '\n' +
+        cachedFiles['task'].content + '\n' +
+        cachedFiles['pagend2'].content;
     
     server.listen(process.env.PORT);
     
